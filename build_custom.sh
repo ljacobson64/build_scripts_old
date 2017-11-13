db7d64a68efde48280bf8d28b28a2c7b789cb6d2
Lucas John Jacobson <ljacobson64@gmail.com>
2017-11-13 12:41:50 -0600

    New build_custom

--- build_custom.sh
+++ build_custom.sh
@@ -1,8 +1,49 @@
 #!/bin/bash
 
-compilers="gcc-6 clang-4.0 intel-17"
-packages="hdf5 moab dagmc"
+compilers=""
+packages=""
 
-for compiler in ${compilers}; do
-  ./install.sh ${compiler} ${packages}
+compilers+=" native"
+compilers+=" gcc-7"
+compilers+=" gcc-6"
+compilers+=" gcc-5"
+compilers+=" gcc-4.9"
+compilers+=" gcc-4.8"
+compilers+=" clang-5.0"
+compilers+=" clang-4.0"
+compilers+=" intel-17"
+compilers+=" intel-16"
+compilers+=" intel-15"
+compilers+=" intel-13"
+
+packages+=" alara"
+packages+=" armadillo"
+packages+=" hdf5"
+packages+=" python"
+packages+=" openmpi"
+packages+=" mcnp"
+packages+=" cgm-12.2"
+packages+=" cgm-13.1"
+packages+=" cgm-14.0"
+packages+=" mcnp2cad-cgm-13.1"
+packages+=" mcnp2cad-cgm-14.0"
+packages+=" moab-4.9.1"
+packages+=" moab-4.9.2"
+packages+=" moab-5.0"
+packages+=" moab-master"
+packages+=" fluka"
+packages+=" geant4"
+packages+=" dagmc"
+packages+=" pyne"
+
+if [[ ${HOSTNAME} == "aci"* ]] || [[ ${HOSTNAME} == "tux"* ]]; then
+  install_script=install_noroot.sh
+else
+  install_script=install.sh
+fi
+
+for package in ${packages}; do
+  for compiler in ${compilers}; do
+    ./${install_script} ${compiler} ${package}
+  done
 done
